236cc987c5b2139443633aaf61703bd8d55d5e70
antbz <antonio.bezerra@fe.up.pt>
2020-05-04 16:23:45 +0100

    Added Graph class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+#include "src/Graph.h"
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Graph<int> graph;
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addEdge(1, 2, 1);
+    graph.addEdge(2, 3, 1);
+    graph.addEdge(3, 1, 1);
+
     return 0;
 }
