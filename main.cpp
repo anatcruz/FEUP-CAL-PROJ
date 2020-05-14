e54c1aa9e195c7f9a18b9532b5c62108b59382d7
andrenasx <andre2000.an@gmail.com>
2020-05-14 18:04:44 +0100

    Showing Porto map

--- main.cpp
+++ main.cpp
@@ -10,25 +10,26 @@ int main() {
 
     srand(time(NULL));
 
-    Graph<coordinates> graph = parseGridMap("../maps/GridGraphs/16x16/nodes.txt", "../maps/GridGraphs/16x16/edges.txt");
+//    Graph<coordinates> graph = parseGridMap("../maps/GridGraphs/16x16/nodes.txt", "../maps/GridGraphs/16x16/edges.txt");
+    Graph<coordinates> graph = parseGridMap("../maps/PortugalMaps/Porto/nodes_x_y_porto.txt", "../maps/PortugalMaps/Porto/edges_porto.txt");
 
 //    path_t generic = graph.dijkstraShortestPath(0, 10);
 //    vector<int> dfs = graph.dfs(9);
-//    vector<int> dij = graph.dijkstraShortestPath(0, );
+//    vector<int> dij = graph.dijkstraShortestPath(0, 42);
 //    vector<int> astar = graph.astarShortestPath(0, 42);
-    vector<int> POIs = {90, 162, 147, 200, 280, 40, 55, 180, 60, 73};
+//    vector<int> POIs = {90, 162, 147, 200, 280, 40, 55, 180, 60, 73};
 //    vector<int> nearest = graph.find_n_nearest(20, POIs, 3);
 //    int near = graph.find_nearest(9, POIs);
-    vector<int> ord;
-    Path path;
-    vector<int> nns = graph.nearestNeighborsSearch(0, 8, POIs, ord, path);
-    Path twop = graph.twoOpt(nns, path);
-
-    ord.clear();
-    POIs = {90, 162, 147, 200, 280, 40, 55, 180, 60, 73};
-    path = Path();
-    vector<int> rnns1 = graph.RNNeighborsSearch(0, 8, POIs, ord, path, 3);
-    Path rtwop = graph.twoOpt(rnns1, path);
+//    vector<int> ord;
+//    Path path;
+//    vector<int> nns = graph.nearestNeighborsSearch(0, 8, POIs, ord, path);
+//    Path twop = graph.twoOpt(nns, path);
+//
+//    ord.clear();
+//    POIs = {90, 162, 147, 200, 280, 40, 55, 180, 60, 73};
+//    path = Path();
+//    vector<int> rnns1 = graph.RNNeighborsSearch(0, 8, POIs, ord, path, 3);
+//    Path rtwop = graph.twoOpt(rnns1, path);
 //    ord.clear();
 //    POIs = {16, 24, 10, 22, 2, 9};
 //    vector<int> rnns2 = graph.RNNeighborsSearch(0, 8, POIs, ord, 2);
@@ -37,8 +38,8 @@ int main() {
     int i=0;
 
     UI ui = UI(graph, 800, 800);
-    ui.showPath(path.getPath());
-//    ui.showGraph();
+//    ui.showPath(path.getPath());
+    ui.showGraph();
 
 #ifndef linux
     char c = getchar();
