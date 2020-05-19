625d1c3ee07afaad5ed638b22f238e23a91de5da
andrenasx <andre2000.an@gmail.com>
2020-05-19 17:17:22 +0100

    added Garage class

---
+++ src/Context/Garage.h
@@ -0,0 +1,21 @@
+#ifndef PROJ_GARAGE_H
+#define PROJ_GARAGE_H
+
+#include <vector>
+#include "Truck.h"
+
+
+class Garage {
+private:
+    int garageNodeID;
+    vector<Truck> trucks;
+
+public:
+    Garage(int garageNodeID);
+    int getGarageNodeID() const;
+    vector<Truck> getTrucks();
+    void addTruck(Truck truck);
+};
+
+
+#endif //PROJ_GARAGE_H
